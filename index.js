55453c2f854d1efa88d50c7250b5b80479563ce4
user_name <email_id>
2022-07-28 19:45:07 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const progressBar = document.querySelectorAll('.progress-bar')
+const about=document.getElementById('clabout')
+about.addEventListener('click', go, false)
+
+
+let num = 0
+
+function addNumber(){
+progressBar.forEach(bar=>{
+         if (num<bar.getAttribute('valuenow')){
+             num+=1
+             bar.style.width = `${num}`+"%"
+             return num;
+        }else{
+             return;
+            }
+    })
+    
+}  
+
+
+function slowAdd(){
+    setInterval(addNumber, 100)
+}
+
+function go(){
+ setTimeout(slowAdd, 500)
+}
